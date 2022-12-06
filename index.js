2ee8d7b239db238fd151c74e0dd3fcf412d27cb0
Jisan-Hasan <jisan.jh5@gmail.com>
2022-12-06 23:08:08 +0600

    save user & update role

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const express = require("express");
 const cors = require("cors");
 const jwt = require("jsonwebtoken");
@@ -12,16 +13,51 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// run function
+// database setup
+const uri = process.env.DB_URI;
+const client = new MongoClient(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    serverApi: ServerApiVersion.v1,
+});
+
 async function run() {
     try {
+        // all collection
+        const usersCollection = client.db("career-club").collection("users");
+
+        // save user in the db
+        app.post("/user", async (req, res) => {
+            const user = req.body;
+            const result = await usersCollection.insertOne(user);
+            res.send({ status: true, data: result });
+        });
+
+        // set User role
+        app.patch("/userRole/:email", async (req, res) => {
+            const email = req.params.email;
+            const role = req.body;
+            const filter = { email: email };
+
+            const options = { upsert: false };
+            const doc = {
+                $set: { role: role.role },
+            };
+            const result = await usersCollection.updateOne(
+                filter,
+                doc,
+                options
+            );
+
+            res.send({ status: true, data: result });
+        });
     } finally {
     }
 }
+run().catch((err) => console.log(err));
 
-run().catch((err) => {
-    console.log(err);
-});
+// all collection
+const usersCollection = client.db("career-club").collection("users");
 
 app.get("/", (req, res) => {
     res.send("Server is running!");
